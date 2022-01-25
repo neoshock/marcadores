0d8e0e75f41f1b784e87ab2cb0fd8ab532f9a35f
neoshock <pideun@gmail.com>
2022-01-24 20:40:49 -0500

    Inicialización de proyecto

---
+++ app/src/main/java/com/example/marcadores/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.marcadores;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng sydney = new LatLng(-34,51);
+        this.googleMap.addMarker(new MarkerOptions().position(sydney).title("Algo Aqui"));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
